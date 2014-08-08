da3dc5450bdabb9ec799eef3693e7c250790fc94
James Cheuk <jccf091@gmail.com>
2014-08-08 14:04:00 +0800

    refacor the login

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,17 @@ class SearchController < ApplicationController
   def index
     keyword = params[:search].strip
 
-    if video = Video.find_by(title: keyword.titleize)
-      redirect_to video
+    if video = find_jump_target keyword
+      redirect_to jump
     else
       @results = Video.search(keyword)
     end
   end
+
+  private
+    def find_jump_target(keyword)
+      Video.find_by(title: keyword.titleize) ||
+      User.find_by(full_name: keyword)
+    end
+
 end
