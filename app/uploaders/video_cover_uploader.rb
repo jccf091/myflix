173bb90688386acfab08f967c94fb9158d72cd25
James Cheuk <jccf091@gmail.com>
2014-08-10 21:14:58 +0800

    create development and production in s3

--- app/uploaders/video_cover_uploader.rb
+++ app/uploaders/video_cover_uploader.rb
@@ -13,7 +13,11 @@ class VideoCoverUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    if Rails.env.production?
+      "production/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    else
+      "development/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   def extension_white_list
