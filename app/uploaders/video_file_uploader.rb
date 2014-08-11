ccebe1431fe7495221ca83d8305ca3bbbebe1970
James Cheuk <jccf091@gmail.com>
2014-08-11 17:45:55 +0800

    feature test admin add new video

--- app/uploaders/video_file_uploader.rb
+++ app/uploaders/video_file_uploader.rb
@@ -8,15 +8,18 @@ class VideoFileUploader < CarrierWave::Uploader::Base
   include ::CarrierWave::Backgrounder::Delay
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  storage :fog
-
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     if Rails.env.production?
       "production/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     else
-      "development/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      "/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
   end
 
