2fdaf57fe6c7f104ffdb72db70bd1fb8824e1887
James Cheuk <jccf091@gmail.com>
2014-08-17 16:47:27 +0800

    payment find by slug

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,7 @@
 class PaymentsController < ApplicationController
   before_action :require_signed_in_user
-  
+
   def show
-    @payments = Payments.find_by(user_id: current_user.id)
+    @payments = Payments.find_by(slug: current_user.id)
   end
 end
